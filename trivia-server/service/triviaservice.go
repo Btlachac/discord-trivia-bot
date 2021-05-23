72082a7b316647849e76a7e3c70100ac124e018c
Brandon Tlachac <BrandonTlachac@gmail.com>
2021-05-22 22:56:46 -0500

    fixed delay times in config.json and corrected the trivia-server volume to only include the audio files

--- trivia-server/service/triviaservice.go
+++ trivia-server/service/triviaservice.go
@@ -60,10 +60,6 @@ func (service *TriviaService) MarkTriviaUsed(triviaId int64) error {
 func writeAudioFile(audioBinary string) (string, error) {
 	audioFileDirectory := os.Getenv("AUDIO_FILE_DIRECTORY")
 
-	if _, err := os.Stat(audioFileDirectory); os.IsNotExist(err) {
-		os.Mkdir(audioFileDirectory, os.ModeDir)
-	}
-
 	uuidWithHyphen := uuid.New()
 	uuid := strings.Replace(uuidWithHyphen.String(), "-", "", -1)
 
