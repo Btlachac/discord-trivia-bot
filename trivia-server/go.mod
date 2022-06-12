a22890476829c43f7dc3ab10861a6f30da51a80d
Brandon Tlachac <BrandonTlachac@gmail.com>
2022-06-11 19:20:38 -0500

    working on adding mocks

--- trivia-server/go.mod
+++ trivia-server/go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/golang-migrate/migrate/v4 v4.14.1
+	github.com/golang/mock v1.6.0 // indirect
 	github.com/google/uuid v1.2.0
 	github.com/gorilla/mux v1.8.0
 	github.com/lib/pq v1.9.0 // indirect
--- trivia-server/postgres/postgres.go
+++ trivia-server/postgres/postgres.go
@@ -1,8 +1,12 @@
 package postgres
 
+import "context"
+
+//go:generate mockgen -source=postgres.go -package=postgres -destination=postgres_mock.go
+
 type TriviaDB interface {
 	GetNewTrivia() (*Trivia, string, error)
-	AddTrivia(newTrivia *Trivia, audioFileName string) error
+	AddTrivia(ctx *context.Context, newTrivia *Trivia, audioFileName string) error
 	MarkTriviaUsed(triviaId int64) error
 	RoundTypesList() ([]*RoundType, error)
 }
