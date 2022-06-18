f9cdd10783d1cd6427988ad81ed6b136f6bf85f3
Brandon Tlachac <BrandonTlachac@gmail.com>
2022-06-18 01:43:04 -0500

    added migration to increase question number limit

---
+++ trivia-server/migrations/3_alter_question_number_limit.up.sql
@@ -0,0 +1,5 @@
+ALTER TABLE dt.question
+DROP CONSTRAINT question_question_number_limit_chk;
+
+ALTER TABLE dt.question
+ADD CONSTRAINT question_question_number_limit_chk CHECK(question_number > 0 AND question_number < 7);
